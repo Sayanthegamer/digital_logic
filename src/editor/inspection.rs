d5c4b86ad9a49db20a058ed25fd7e5c41a1ddff4
Anon <anon@example.com>
2026-06-30 00:20:43 +0530

    Drop Android support, restore Rust 2024 edition and let-chains syntax on main

--- src/editor/inspection.rs
+++ src/editor/inspection.rs
@@ -50,11 +50,10 @@ impl Editor {
                     let inputs: Vec<&super::types::VisualComponent> = self.components.iter()
                         .filter(|c| c.comp_type == ComponentType::Input)
                         .collect();
-                    if let Some(comp) = inputs.get(*idx) {
-                        if let Some(&g_idx) = self.visual_to_sim_map.get(&comp.id) {
+                    if let Some(comp) = inputs.get(*idx)
+                        && let Some(&g_idx) = self.visual_to_sim_map.get(&comp.id) {
                             return self.simulator.get_state(g_idx);
                         }
-                    }
                 }
                 TraceNode::CompOutput { component_idx, port_idx } => {
                     if let Some(&g_idx) = self.port_to_sim_gate_map.get(&(*component_idx, *port_idx)) {
